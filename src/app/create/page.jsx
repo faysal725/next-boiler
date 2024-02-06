564690173f21497a612c0d270acc9f8be9eb0dc9
faysal725 <afaysal725@gmail.com>
2024-02-06 09:17:42 +0600

    rich text editoradded

--- src/app/create/page.jsx
+++ src/app/create/page.jsx
@@ -5,19 +5,23 @@ import React, { useEffect, useState } from 'react'
 import Form from '../components/Form/Form'
 import FormInput from '../components/Inputs/FormInput'
 import Dropdown from '../components/Inputs/Dropdown'
+import Ckeditor from '../components/Inputs/Ckeditor'
+import PrimaryBtn from '../components/Button/PrimaryBtn'
 
 
 export default function Create() {
 
   const [userData, setUserData] = useState({})
-  useEffect(() => {
-    console.log(userData)
-  
-      }, [userData]);
+
+
+
+  const submitData = () => {
+    console.log(userData, 'asdfsdf')
+  }
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="w-96" >
-          <Form title="User Info">
+          <Form title="Insert User Info">
             
               <label className="block">
                   <FormInput changedValue={(name) => setUserData({...userData, "name":name })} type="text" label="name"/>
@@ -28,10 +32,6 @@ export default function Create() {
               </label>
 
 
-              {/* <label className="block">
-                  <FormInput changedValue={(description) => setUserData({...userData, "description":description })} type="date" label="description"/>
-              </label> */}
-
               <label className="block">
                   <FormInput changedValue={(birthdate) => setUserData({...userData, "birthdate":birthdate })} type="date" label="Birthdate" />
               </label>
@@ -40,7 +40,7 @@ export default function Create() {
                   <Dropdown label="Status" changedValue={(active_status) => setUserData({...userData, "active_status":active_status })} />
               </label>
 
-
+              
               {/* <label className="block border border-black  text-xs p-3">
                   <Dropzone onDrop={acceptedFiles => setUserData({...userData, "profile_picture":acceptedFiles })}>
                       {({getRootProps, getInputProps}) => (
@@ -56,7 +56,12 @@ export default function Create() {
               
 
               {/* <ButtonPrimary  type="submit" title="Submit">Continue</ButtonPrimary> */}
+              <button onClick={() => submitData()}>Submit</button>
+              <PrimaryBtn onClick={() => submitData} title="Submit" />
           </Form>
+      </div>
+      <div className='mt-10'>
+          <Ckeditor title="User Details" editorData={(description) => setUserData({...userData, "description":description })}/>
       </div>
     </main>
   )
